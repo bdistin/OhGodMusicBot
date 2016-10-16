9cf7f3d3bd938c88b61fa1191f107513a488d5b5
bdistin <bdistin@gmail.com>
2016-10-16 12:44:34 -0500

    Fixed a small bug with mention prefixes

--- OhGodMusicBot/add.js
+++ OhGodMusicBot/add.js
@@ -1,13 +1,11 @@
 const yt = require("ytdl-core");
 
-exports.run = (client, msg) => {
+exports.run = (client, msg, [song]) => {
   if(!client.hasOwnProperty("queue")) client.queue = {};
-  let url = msg.content.split(" ")[1];
-  if (url == "" || url === undefined) return msg.channel.sendMessage(`You must add a url, or youtube video id after ${client.config.prefix}add`);
-  yt.getInfo(url, (err, info) => {
+  yt.getInfo(song, (err, info) => {
     if(err) return msg.channel.sendMessage("Invalid YouTube Link: " + err);
     if (!client.queue.hasOwnProperty(msg.guild.id)) client.queue[msg.guild.id] = {}, client.queue[msg.guild.id].playing = false, client.queue[msg.guild.id].songs = [];
-    client.queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
+    client.queue[msg.guild.id].songs.push({url: song, title: info.title, requester: msg.author.username});
     msg.channel.sendMessage(`added **${info.title}** to the queue`);
   });
 };
@@ -24,6 +22,6 @@ exports.conf = {
 exports.help = {
   name: "add",
   description: "Adds a song the the queue.",
-  usage: "",
+  usage: "<song:str>",
   usageDelim: ""
 };
