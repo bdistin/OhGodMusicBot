08ffcb9dc959e59ae2428ecb8e7c4b092d619edd
bdistin <bdistin@gmail.com>
2016-12-31 09:58:18 -0600

    OhGodRadioBot Rough & Buggy initial release

--- server.js
+++ server.js
@@ -3,97 +3,109 @@ const yt = require('ytdl-core');
 const tokens = require('./tokens.json');
 const client = new Discord.Client();
 
-let queue = {};
+let broadcasts = new Discord.Collection();
 
 const commands = {
-	'play': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
-		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
-		if (queue[msg.guild.id].playing) return msg.channel.sendMessage('Already Playing');
-		let dispatcher;
-		queue[msg.guild.id].playing = true;
-
-		console.log(queue);
-		(function play(song) {
-			console.log(song);
-			if (song === undefined) return msg.channel.sendMessage('Queue is empty').then(() => {
-				queue[msg.guild.id].playing = false;
-				msg.member.voiceChannel.leave();
-			});
-			msg.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
-			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : tokens.passes });
-			let collector = msg.channel.createCollector(m => m);
+	'broadcast': (msg) => {
+		join(msg).then(() => {
+			if (msg.guild.broadcast) return msg.reply('You are already broadcasting...');
+			broadcasts.set(msg.guild.id, {stream: client.createVoiceBroadcast(),listeners: new Discord.Collection(),guild: msg.guild,queue: {playing: false, songs: []}});
+			let broadcast = msg.guild.broadcast = broadcasts.get(msg.guild.id);
+			broadcast.listeners.set(msg.guild.id, {voiceChannel: msg.member.voiceChannel,textChannel: msg.channel});
+			let collector = broadcast.collector = msg.channel.createCollector(m => m);
 			collector.on('message', m => {
-				if (m.content.startsWith(tokens.prefix + 'pause')) {
-					msg.channel.sendMessage('paused').then(() => {dispatcher.pause();});
-				} else if (m.content.startsWith(tokens.prefix + 'resume')){
-					msg.channel.sendMessage('resumed').then(() => {dispatcher.resume();});
-				} else if (m.content.startsWith(tokens.prefix + 'skip')){
-					msg.channel.sendMessage('skipped').then(() => {dispatcher.end();});
-				} else if (m.content.startsWith('volume+')){
-					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
-					msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-				} else if (m.content.startsWith('volume-')){
-					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
-					msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-				} else if (m.content.startsWith(tokens.prefix + 'time')){
-					msg.channel.sendMessage(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
-				}
+				if (!m.content.startsWith(tokens.prefix)) return;
+				if (broadcastCommands.hasOwnProperty(m.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) broadcastCommands[m.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](m, broadcast);
 			});
-			dispatcher.on('end', () => {
-				collector.stop();
-				queue[msg.guild.id].songs.shift();
-				play(queue[msg.guild.id].songs[0]);
-			});
-			dispatcher.on('error', (err) => {
-				return msg.channel.sendMessage('error: ' + err).then(() => {
-					collector.stop();
-					queue[msg.guild.id].songs.shift();
-					play(queue[msg.guild.id].songs[0]);
-				});
-			});
-		})(queue[msg.guild.id].songs[0]);
+		}).catch(() => msg.reply('I couldn\'t connect to your voice channel...'));
 	},
-	'join': (msg) => {
-		return new Promise((resolve, reject) => {
-			const voiceChannel = msg.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-		});
-	},
-	'add': (msg) => {
-		let url = msg.content.split(' ')[1];
-		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a url, or youtube video id after ${tokens.prefix}add`);
-		yt.getInfo(url, (err, info) => {
-			if(err) return msg.channel.sendMessage('Invalid YouTube Link: ' + err);
-			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
-			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
-			msg.channel.sendMessage(`added **${info.title}** to the queue`);
+	'tune-in': (msg) => {
+		if (broadcasts.size === 0) return msg.reply(`Nobody is streaming right now. Broadcast your own stream with ${tokens.prefix}broadcast`);
+		if (broadcasts.has(msg.guild.id)) deleteBroadcast(broadcasts.get(msg.guild.id));
+		if (msg.guild.broadcast) {
+			if(msg.guild.broadcast.guild.id === msg.guild.id) deleteBroadcast(msg.guild.broadcast);
+			else msg.guild.broadcast.listeners.delete(msg.guild.id);
+			msg.guild.broadcast = undefined;
+		}
+		let broadcastsList = broadcasts.map(b => { return {guildID: b.guild.id, guildName: b.guild.name, song: b.queue.current.title, tunedIn: b.listeners.size, listeners: b.listeners.reduce((a,b) => a+b.voiceChannel.members.size, 0)}; });
+		let broadcastsListFriendly = [];
+		for (let i = 0; i < broadcasts.size; i++) { broadcastsListFriendly.push(`__**${i}**__ - ${broadcastsList[i].guildName}'s broadcast - ${broadcastsList[i].tunedIn} guilds tuned in - ${broadcastsList[i].listeners} listening\nCurrently Playing: ${broadcastsList[i].song}\n`); }
+		msg.channel.sendMessage('Respond "random", or the number from the following list.\n'+broadcastsListFriendly.join('\n')).then(() => {
+			msg.channel.awaitMessages(m => m.author === msg.author, { max: 1, time: 60000, errors: ['time'] }).then(collected => {
+				let broadcast;
+				if (!isNaN(parseInt(collected.first().content.toLowerCase())) && parseInt(collected.first().content.toLowerCase()) < broadcasts.size) broadcast = msg.guild.broadcast = broadcasts.get(broadcastsList[parseInt(collected.first().content.toLowerCase())].guildID);
+				else if (collected.first().content.toLowerCase() === 'random') broadcast = msg.guild.broadcast = broadcasts.random();
+				else return msg.channel.sendMessage('Unknown broadcast...');
+				join(msg).then(voiceConnection => {
+					voiceConnection.playBroadcast(broadcast.stream);
+					broadcast.listeners.set(msg.guild.id, {voiceChannel: msg.member.voiceChannel,textChannel: msg.channel});
+				}).catch(() => msg.reply('I couldn\'t connect to your voice channel...'));
+			}).catch(() => msg.channel.sendMessage('I am sorry, the request has timed out.'));
 		});
 	},
 	'queue': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
+		if (!msg.guild.broadcast) return msg.channel.sendMessage(`You are neither broadcasting, or Tuned In to a broadcast. Use ${tokens.prefix}broadcast or ${tokens.prefix}tune-in to get started.`);
+		if (msg.guild.broadcast.queue.songs.length === 0 && msg.guild.id === msg.guild.broadcast.guild.id) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
+		else if (msg.guild.broadcast.queue.songs.length === 0 && msg.guild.id !== msg.guild.broadcast.guild.id) return msg.channel.sendMessage('The broadcast is playing the last song in it\'s queue.');
 		let tosend = [];
-		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
-		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+		msg.guild.broadcast.queue.songs.forEach((song, i) => {tosend.push(`${i + 1}. ${song.title} - Requested by: ${song.requester}`);});
+		msg.channel.sendMessage(`__**${msg.guild.broadcast.guild.name}'s Radio Broadcast:**__ Currently **${tosend.length}** songs queued ${ (tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0, 15).join('\n')}\`\`\``);
 	},
 	'help': (msg) => {
-		let tosend = ['```xl', tokens.prefix + 'join : "Join Voice channel of msg sender"',	tokens.prefix + 'add : "Add a valid youtube link to the queue"', tokens.prefix + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause : "pauses the music"',	tokens.prefix + 'resume : "resumes the music"', tokens.prefix + 'skip : "skips the playing song"', tokens.prefix + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
-		msg.channel.sendMessage(tosend.join('\n'));
+		msg.channel.sendMessage(['```xl', tokens.prefix + 'join : "Join Voice channel of msg sender"',tokens.prefix + 'add : "Add a valid youtube link to the queue"',tokens.prefix + 'queue : "Shows the current queue, up to 15 songs shown."',tokens.prefix + 'play : "Play the music queue if already joined to a voice channel"','','the following commands only function while the play command is running:'.toUpperCase(),tokens.prefix + 'pause : "pauses the music"',tokens.prefix + 'resume : "resumes the music"',tokens.prefix + 'skip : "skips the playing song"',tokens.prefix + 'time : "Shows the playtime of the song."','volume+(+++) : "increases volume by 2%/+"','volume-(---) : "decreases volume by 2%/-"','```']); //completely outdated, but low priority
 	},
 	'reboot': (msg) => {
 		if (msg.author.id == tokens.adminID) process.exit(); //Requires a node module like Forever to work.
 	}
 };
 
+const broadcastCommands = {
+	'play': (msg, broadcast) => {
+		if (broadcast.queue.playing) return msg.channel.sendMessage('Already Playing');
+		if (broadcast.queue.length === 0) return msg.channel.sendMessage(`The queue is empty, try adding a song with ${tokens.prefix}add`);
+		broadcast.queue.playing = true;
+		let dispatcher;
+		(function play(song) {
+			console.log(song);
+			if (song === undefined) return msg.channel.sendMessage('Queue is empty').then(() => { deleteBroadcast(broadcast); });
+			msg.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
+			dispatcher = msg.guild.voiceConnection.playBroadcast(broadcast.stream.playStream(yt(song.url, {audioonly: true}), {passes: tokens.passes}));
+			dispatcher.on('end', () => { play(broadcast.queue.current = broadcast.queue.songs.shift()); });
+			dispatcher.on('error', (err) => {
+				return msg.channel.sendMessage('error: ' + err).then(() => {
+					play(broadcast.queue.current = broadcast.queue.songs.shift());
+				});
+			});
+		})(broadcast.queue.current = broadcast.queue.songs.shift());
+	},
+	'add': (msg, broadcast) => {
+		let url = msg.content.split(' ')[1];
+		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a url, or youtube video id after ${tokens.prefix}add`);
+		yt.getInfo(url, (err, info) => {
+			if (err) return msg.channel.sendMessage('Invalid YouTube Link: ' + err);
+			broadcast.queue.songs.push({ url: url, title: info.title, requester: msg.author.username });
+			msg.channel.sendMessage(`added **${info.title}** to the queue`);
+		});
+	}
+};
+
+const join = (msg) => {
+	return new Promise((resolve, reject) => msg.member.voiceChannel.join().then(resolve).catch(reject));
+};
+
+const deleteBroadcast = (broadcast) => {
+	broadcast.collector.stop();
+	broadcast.listeners.forEach(l => l.voiceChannel.leave());
+	broadcasts.delete(broadcast.guild.id);
+};
+
 client.on('ready', () => {
-	console.log('ready!');
+	console.log('ready');
 });
 
 client.on('message', msg => {
 	if (!msg.content.startsWith(tokens.prefix)) return;
 	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
 });
+
 client.login(tokens.d_token);
