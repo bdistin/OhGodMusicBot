e77f1588a1aeedb45dce7808f7849e09f9eccfa4
bdistin <bdistin@gmail.com>
2016-12-31 10:57:00 -0600

    Tune-Out and broadcast clearing

--- server.js
+++ server.js
@@ -13,20 +13,12 @@ const commands = {
 			let broadcast = msg.guild.broadcast = broadcasts.get(msg.guild.id);
 			broadcast.listeners.set(msg.guild.id, {voiceChannel: msg.member.voiceChannel,textChannel: msg.channel});
 			let collector = broadcast.collector = msg.channel.createCollector(m => m);
-			collector.on('message', m => {
-				if (!m.content.startsWith(tokens.prefix)) return;
-				if (broadcastCommands.hasOwnProperty(m.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) broadcastCommands[m.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](m, broadcast);
-			});
+			collector.on('message', m => { if (m.content.startsWith(tokens.prefix) && broadcastCommands.hasOwnProperty(m.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) broadcastCommands[m.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](m, broadcast); });
 		}).catch(() => msg.reply('I couldn\'t connect to your voice channel...'));
 	},
 	'tune-in': (msg) => {
 		if (broadcasts.size === 0) return msg.reply(`Nobody is streaming right now. Broadcast your own stream with ${tokens.prefix}broadcast`);
-		if (broadcasts.has(msg.guild.id)) deleteBroadcast(broadcasts.get(msg.guild.id));
-		if (msg.guild.broadcast) {
-			if(msg.guild.broadcast.guild.id === msg.guild.id) deleteBroadcast(msg.guild.broadcast);
-			else msg.guild.broadcast.listeners.delete(msg.guild.id);
-			msg.guild.broadcast = undefined;
-		}
+		clearBroadcast(msg);
 		let broadcastsList = broadcasts.map(b => { return {guildID: b.guild.id, guildName: b.guild.name, song: b.queue.current.title, tunedIn: b.listeners.size, listeners: b.listeners.reduce((a,b) => a+b.voiceChannel.members.size, 0)}; });
 		let broadcastsListFriendly = [];
 		for (let i = 0; i < broadcasts.size; i++) { broadcastsListFriendly.push(`__**${i}**__ - ${broadcastsList[i].guildName}'s broadcast - ${broadcastsList[i].tunedIn} guilds tuned in - ${broadcastsList[i].listeners} listening\nCurrently Playing: ${broadcastsList[i].song}\n`); }
@@ -43,10 +35,11 @@ const commands = {
 			}).catch(() => msg.channel.sendMessage('I am sorry, the request has timed out.'));
 		});
 	},
+	'tune-out': (msg) => { clearBroadcast(msg, true); },
 	'queue': (msg) => {
 		if (!msg.guild.broadcast) return msg.channel.sendMessage(`You are neither broadcasting, or Tuned In to a broadcast. Use ${tokens.prefix}broadcast or ${tokens.prefix}tune-in to get started.`);
 		if (msg.guild.broadcast.queue.songs.length === 0 && msg.guild.id === msg.guild.broadcast.guild.id) return msg.channel.sendMessage(`Add some songs to the queue first with ${tokens.prefix}add`);
-		else if (msg.guild.broadcast.queue.songs.length === 0 && msg.guild.id !== msg.guild.broadcast.guild.id) return msg.channel.sendMessage('The broadcast is playing the last song in it\'s queue.');
+		else if (msg.guild.broadcast.queue.songs.length === 0 && msg.guild.id !== msg.guild.broadcast.guild.id) return msg.channel.sendMessage(`The broadcast is playing the last song in it\'s queue:\n${msg.guild.broadcast.queue.current.title}`);
 		let tosend = [];
 		msg.guild.broadcast.queue.songs.forEach((song, i) => {tosend.push(`${i + 1}. ${song.title} - Requested by: ${song.requester}`);});
 		msg.channel.sendMessage(`__**${msg.guild.broadcast.guild.name}'s Radio Broadcast:**__ Currently **${tosend.length}** songs queued ${ (tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0, 15).join('\n')}\`\`\``);
@@ -64,15 +57,15 @@ const broadcastCommands = {
 		if (broadcast.queue.playing) return msg.channel.sendMessage('Already Playing');
 		if (broadcast.queue.length === 0) return msg.channel.sendMessage(`The queue is empty, try adding a song with ${tokens.prefix}add`);
 		broadcast.queue.playing = true;
-		let dispatcher;
 		(function play(song) {
 			if (song === undefined) return msg.channel.sendMessage('Queue is empty').then(() => { deleteBroadcast(broadcast); });
 			msg.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
-			dispatcher = msg.guild.voiceConnection.playBroadcast(broadcast.stream.playStream(yt(song.url, {audioonly: true}), {passes: tokens.passes}));
+			let dispatcher = msg.guild.voiceConnection.playBroadcast(broadcast.stream.playStream(yt(song.url, {audioonly: true}), {passes: tokens.passes}));
 			dispatcher.on('end', () => { play(broadcast.queue.current = broadcast.queue.songs.shift()); });
 			dispatcher.on('error', (err) => { return msg.channel.sendMessage('error: ' + err).then(() => { play(broadcast.queue.current = broadcast.queue.songs.shift()); }); });
 		})(broadcast.queue.current = broadcast.queue.songs.shift());
 	},
+	'end': (msg, broadcast) => { deleteBroadcast(broadcast); },
 	'add': (msg, broadcast) => {
 		let url = msg.content.split(' ')[1];
 		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a url, or youtube video id after ${tokens.prefix}add`);
@@ -90,11 +83,18 @@ const deleteBroadcast = (broadcast) => {
 	broadcasts.delete(broadcast.guild.id);
 };
 
+const clearBroadcast = (msg, leave = false) => {
+	if (!msg.guild.broadcast) return;
+	if (msg.guild.broadcast.guild.id === msg.guild.id) deleteBroadcast(msg.guild.broadcast);
+	else {
+		if (leave) msg.guild.broadcast.listeners.get(msg.guild.id).voiceChannel.leave();
+		msg.guild.broadcast.listeners.delete(msg.guild.id);
+	}
+	msg.guild.broadcast = undefined;
+};
+
 client.on('ready', () => { console.log('ready'); });
 
-client.on('message', msg => {
-	if (!msg.content.startsWith(tokens.prefix)) return;
-	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg);
-});
+client.on('message', msg => { if (msg.content.startsWith(tokens.prefix) && commands.hasOwnProperty(msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(tokens.prefix.length).split(' ')[0]](msg); });
 
 client.login(tokens.d_token);
